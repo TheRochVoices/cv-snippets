9cf6fda22b6e52b82876fdfbe1ac6610afe23ec2
TheRochVoices <rochaks001@gmail.com>
2018-06-20 21:52:22 +0530

    TBA

--- greenScreen.py
+++ greenScreen.py
@@ -1,12 +1,17 @@
 import cv2 as cv
-
+import numpy as np
 cap = cv.VideoCapture('test.mp4')
-
+img = cv.imread('house.jpg')
 while True:
 	ret, img = cap.read()
-
-
-	cv.imshow('some', img)
+	hued = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+	lower = np.array([0,0,0])
+	upper = np.array([255,255,0])
+	mask = cv.inRange(hued, lower, upper)
+	mask = cv.bitwise_not(mask)
+	#res = img + mask
+	#not done
+	cv.imshow('some', res)
 	if 0xFF & cv.waitKey(5) == 27:
 		break
 cv.destroyAllWindows()
